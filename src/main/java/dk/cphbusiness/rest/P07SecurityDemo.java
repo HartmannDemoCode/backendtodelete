277d1c0358067a22b1104c950140451d0747aca1
Thomas Hartmann <thomas@webtrade.dk>
2023-12-05 21:51:03 +0100

    added cors

--- src/main/java/dk/cphbusiness/rest/P07SecurityDemo.java
+++ src/main/java/dk/cphbusiness/rest/P07SecurityDemo.java
@@ -1,7 +1,13 @@
 package dk.cphbusiness.rest;
 
+import dk.cphbusiness.controllers.PersonController;
 import dk.cphbusiness.data.HibernateConfig;
+import dk.cphbusiness.security.SecurityController;
 import dk.cphbusiness.security.SecurityRoutes;
+import io.javalin.apibuilder.EndpointGroup;
+
+import static dk.cphbusiness.security.SecurityRoutes.Role;
+import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class P07SecurityDemo {
     // 1. Hashing of passwords in security.User
@@ -11,15 +17,30 @@ public class P07SecurityDemo {
     // 5. SecurityRoutes (auth and protected)
     // 6. SecurityTest with Login and token send to protected
     public static void main(String[] args) {
+        PersonController personController = new PersonController();
         ApplicationConfig
                 .getInstance()
                 .initiateServer()
                 .setRoutes(SecurityRoutes.getSecurityRoutes())
                 .setRoutes(SecurityRoutes.getSecuredRoutes())
+                .setRoutes(getPersonRessource())
+                .setCORS()
                 .startServer(7007)
                 .checkSecurityRoles()
                 .setGeneralExceptionHandling()
                 .setErrorHandling()
                 .setApiExceptionHandling();
+    }
+
+    private static EndpointGroup getPersonRessource() {
+        PersonController personController = new PersonController();
+        SecurityController securityController = SecurityController.getInstance();
+        return () -> {
+            path("/person", () -> {
+                before(securityController.authenticate());
+                get("/", personController.getAll(), Role.USER);
+                get("/{id}", personController.getById(), Role.USER);
+            });
+        };
     }
 }
